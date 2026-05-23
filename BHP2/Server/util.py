9d4e07fa5585009042e2715967f80c420be82d57
anweshkumarsv <anweshkumarsv@gmail.com>
2026-05-23 12:14:17 +0530

    Add input validation with helpful error messages

--- BHP2/Server/util.py
+++ BHP2/Server/util.py
@@ -7,27 +7,45 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,bhk,bath):
-    try:
-        loc_index = __data_columns.index(location.lower())
-    except:
-        loc_index = -1
+def get_estimated_price(location, sqft, bhk, bath):
+    # Validate inputs
+    if sqft < 100:
+        raise ValueError("Area must be at least 100 sq ft")
+    if sqft > 50000:
+        raise ValueError("Area cannot exceed 50,000 sq ft")
+    if bhk < 1 or bhk > 10:
+        raise ValueError("BHK must be between 1 and 10")
+    if bath < 1 or bath > 10:
+        raise ValueError("Bathrooms must be between 1 and 10")
+    if not location or location.strip() == "":
+        raise ValueError("Please select a valid location")
+    
+    # Find location in the list (case-insensitive)
+    loc_index = -1
+    if __locations:
+        for i, loc in enumerate(__locations):
+            if loc.lower() == location.lower():
+                loc_index = i + 3  # +3 because first 3 columns are sqft, bath, bhk
+                break
+    
+    if loc_index < 0:
+        raise ValueError(f"Location '{location}' not found in our database")
 
     x = np.zeros(len(__data_columns))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index>=0:
+    if loc_index >= 0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0],2)
+    return round(__model.predict([x])[0], 2)
 
 def get_location_names():
     return __locations
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
-    global  __data_columns
+    global __data_columns
     global __locations
     global __model
 
@@ -57,7 +75,7 @@ def get_data_columns():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
     print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
-    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))
+    print(get_estimated_price('Ejipura', 1000, 2, 2))
